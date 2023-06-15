860c5ac34926bf267d95c95b9d9d05ef4c8c4147
Bryson Kern <kernbryson@yahoo.com>
2023-06-15 16:44:39 -0400

    quiz now gives percentage of how many you answered correctly

--- cert-a-pro/src/app/quiz/quiz.component.ts
+++ cert-a-pro/src/app/quiz/quiz.component.ts
@@ -10,6 +10,8 @@ import { getFirestore, collection, query, where, getDocs, DocumentData, QueryDoc
 export class QuizComponent implements OnInit {
   certificationID: string | null = null;
   questions: any[] = [];
+  selectedAnswers: any[] = [];
+  correctCount: number = 0;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -26,16 +28,61 @@ export class QuizComponent implements OnInit {
     const firestore = getFirestore();
     const questionsQuery = query(collection(firestore, 'questions'), where('certificationID', '==', this.certificationID));
     const querySnapshot = await getDocs(questionsQuery);
-
+  
     this.questions = querySnapshot.docs.map((doc: QueryDocumentSnapshot<DocumentData>) => {
       const questionData = doc.data();
       questionData['answers'] = JSON.parse(questionData['answers']); // Parse the answers field
       questionData['selectedAnswer'] = null; // Initialize selected answer
+  
+      // Calculate correctCount based on correct answers
+      questionData['correctCount'] = questionData['answers'].filter((answer: any) => answer.correctAnswer).length;
+  
       return questionData;
     });
   }
+  
 
   selectAnswer(question: any, answer: any) {
     question.selectedAnswer = answer;
   }
+  submitQuiz() {
+    this.selectedAnswers = [];
+    let correctAnswersCount = 0; // Initialize count of correct answers
+    for (const question of this.questions) {
+      if (question.selectedAnswer) {
+        this.selectedAnswers.push({
+          question: question.question,
+          answer: question.selectedAnswer.answer
+        });
+  
+        if (question.selectedAnswer.correctAnswer) {
+          correctAnswersCount++; // Increment count for each correct answer
+        }
+      }
+    }
+    console.log(this.selectedAnswers);
+  
+    const percentage = (correctAnswersCount / this.questions.length) * 100; // Calculate percentage
+  
+    console.log(`Percentage of correct answers: ${percentage}%`);
+  
+    this.correctCount = correctAnswersCount; // Update the correctCount property
+  
+    // You may also choose to update the correctCount property for each question in the questions array, similar to the previous implementation
+  
+    // Update the correctCount property for each question
+    for (const question of this.questions) {
+      question.correctCount = question.answers.filter((answer: any) => answer.correctAnswer).length;
+    }
+  }
+  
+  
+  calculatePercentage(): number {
+    const percentage = (this.correctCount > 0) ? (this.correctCount / this.questions.length) * 100 : 0;
+    return Math.round(percentage);
+  }
+  
+  
+  
+
 }
